f177c1b94af90a06c46f504ce9d78625fdfb7503
Joe D'Amore <joe@joedamore.me>
2025-05-29 08:08:00 -0400

    Add hello-gilrs code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use gilrs::{Gilrs, Button, Event};
+
 fn main() {
-    println!("Hello, world!");
+    let mut gilrs = Gilrs::new().unwrap();
+
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
+
+    let mut active_gamepad = None;
+
+    loop {
+        while let Some(Event { id, event, time, .. }) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", time, id, event);
+            active_gamepad = Some(id);
+        }
+    }
 }
